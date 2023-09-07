9a902edf0f628e69741358dee17c2fb3fc6e7831
Rishab Bahal <rishabbahal@gmail.com>
2023-09-07 11:22:24 -0400

    separated serverfile from app.

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
-const mongoose = require("mongoose");
 const logger = require("./util/logger");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
-const ENV = process.env.ENV;
-const ENABLE_DEBUG = process.env.ENABLE_DEBUG;
-const ENABLE_EMAIL = process.env.ENABLE_EMAIL;
-const mongoDbUrl = process.env.MONGODB_URL;
 
 const v0Routes = require("./api/v0/index");
 const v1Routes = require("./api/v1/index");
@@ -53,20 +47,4 @@ app.use("/v0", v0Routes.router);
 app.use("/v1", v1Routes.router);
 app.use("/", swaggerUi.serve, swaggerUi.setup(null, options));
 
-mongoose
-  .connect(mongoDbUrl, { useNewUrlParser: true })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.group("Environment details");
-      console.table({
-        "Environment": ENV,
-        "Debugging enabled": ENABLE_DEBUG,
-        "Email enabled": ENABLE_EMAIL,
-        Port: PORT,
-      });
-      console.groupEnd();
-    });
-  })
-  .catch((err) => {
-    logger.error(err);
-  });
+module.exports = app;
